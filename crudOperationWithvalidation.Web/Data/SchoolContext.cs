28d18fd181b1f67797d51ef7844814334360b53e
PERCOID-IT <PERCOID-IT@DESKTOP-SANISHA>
2024-06-24 15:30:36 +0545

    Add project files.

---
+++ crudOperationWithvalidation.Web/Data/SchoolContext.cs
@@ -0,0 +1,52 @@
+﻿using crudOperationWithvalidation.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace crudOperationWithvalidation.Web.Data
+{
+    public class SchoolContext : DbContext
+    {
+
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Teacher> Teachers { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+            }
+        }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Teacher>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Skills)
+                    .IsRequired()
+                    .HasMaxLength(250)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Salary)
+                    .IsRequired()
+                    .HasColumnType("money");
+
+                entity.Property(e => e.AddedOn)
+                    .HasColumnType("date")
+                    .HasDefaultValueSql("(getdate())");
+            });
+
+        }
+
+    }
+
+
+}
---
+++ crudOperationWithvalidation.Web/Models/Teacher.cs
@@ -0,0 +1,29 @@
+﻿using crudOperationWithvalidation.Web.CustomValidation;
+using System.ComponentModel.DataAnnotations;
+
+namespace crudOperationWithvalidation.Web.Models
+{
+    public class Teacher
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [SkillsValidate(Allowed = new string[] { "ASP.NET Core", "ASP.NET MVC", "ASP.NET Web Forms" }, ErrorMessage = "You skills are invalid")]
+        public string Skills {  get; set; }
+
+        [Range(5, 50)]
+        public  int TotalStudent { get; set; }
+
+        [Required]
+        public decimal Salary {  get; set; }
+
+
+        [Required]
+        public DateTime AddedOn { get; set; }
+
+    }
+}
